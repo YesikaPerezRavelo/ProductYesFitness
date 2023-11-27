452264f872e86e20ab46e4b35fc2c97e18243592
Yesika Perez Ravelo <yesikapr@gmail.com>
2023-11-26 22:49:22 -0300

    organizando

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,29 @@ import viteLogo from "/vite.svg";
 import ItemCount from "./components/ItemCount/ItemCount.jsx";
 import NavBar from "./components/Navbar/NavBar.jsx";
 
+const Layout = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+      {props.children}
+    </div>
+  );
+};
+
 function App() {
   return (
     <>
       <NavBar />
 
-      <h1>Vive la experiencia más única e inigualable</h1>
+      <Layout
+        title={"Vive la experiencia más única e inigualable"}
+        color="Dark"
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, earum.
+        </p>
+      </Layout>
 
       <ItemCount />
     </>
