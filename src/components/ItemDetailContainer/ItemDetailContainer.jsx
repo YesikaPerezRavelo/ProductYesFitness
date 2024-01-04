9e1404c64b574a4206e9d20afcea35d108765c46
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-01-04 20:31:57 -0300

    error

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,18 +1,43 @@
 import { useState, useEffect } from "react";
-import { getProductById } from "../../asyncMock";
+// import { getProductById } from "../../asyncMock";
 import { useParams } from "react-router-dom";
 import ItemDetail from "../ItemDetail/ItemDetail";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../services/firebase/firebaseConfig";
 
 const ItemDetailContainer = () => {
+  const [loading, setLoading] = useState(true);
   const [product, setProduct] = useState(null);
 
   const { productId } = useParams();
 
   useEffect(() => {
-    getProductById(productId).then((response) => {
-      setProduct(response);
-    });
+    setLoading(true);
+
+    const documentRef = doc(db, "products", productId);
+
+    getDoc(documentRef)
+      .then((QueryDocumentSnapshot) => {
+        const fields = QueryDocumentSnapshot.data();
+        const productsAdapted = { id: QueryDocumentSnapshot.id, ...fields };
+        setProduct(productsAdapted);
+      })
+
+      .cath((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+
+    // getProductById(productId).then((response) => {
+    //   setProduct(response);
+    // });
   }, [productId]);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
